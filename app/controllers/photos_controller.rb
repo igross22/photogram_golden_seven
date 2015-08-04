9c4c137a46e104815fa426a9c5c48515fd6400bf
igross22 <ian.j.gross@gmail.com>
2015-08-04 17:33:58 -0500

    Changes as of 8/4/15

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,37 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def edit
+
+    @photo = Photo.find(params["id"])
+
+    render("edit.html.erb")
+
+  end
+
+  def destroy
+
+    @photo = Photo.find(params["id"])
+    @photo.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def new
+
+    p = Photo.new
+    p.caption = params["the_caption"]
+    p.source = params["the_source"]
+    p.save
+
+
+  end
+
+
+  def show
+
+    @photo = Photo.find(params["id"])
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
+  get("/photos/new",       { :controller => "photos", :action => "new" })
+  get("/photos/:id/edit",  { :controller => "photos", :action => "edit" })
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/delete_photo/:id",    { :controller => "photos", :action => "destroy" })
 
 end
+
